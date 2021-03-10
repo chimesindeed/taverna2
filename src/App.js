dfd7263528ccf0bbcec6ae2c6f69130763e92516
dimitri.gedda <gratitudeadore@fastmail.com>
2021-03-10 04:55:40 -0500

    GET Request loads data into state variable items. UseEffect used as ComponentDidMount

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
+import {MenuItems} from './MenuItems'
 
-const url = "localhost:3001/menus";
+const url = "http://localhost:3001/menus";
+  
 export default function App() {
   const [items, setItems]=useState("");
-
-  const renderItems = () => {
-    if(items ===""){return "loading"
-    }
-    else {
-      return(
-        <ul>
-        {
-          items.map((item)=>{
-          return(<li>item.item</li>)
-        })
-        }
-        </ul>
-      )
-    }
-
+  
+  function getItems(){
+    fetch(url)
+      .then((res)=>{return res.json()})
+        .then((res)=>{return setItems(res)})
   }
+
+  useEffect(()=>{ return (getItems(), []) })
+  
   return (
     <div className="main">
       <div className="container">
-        <div className="header">
-          <div className="logo">Taverna |</div>
 
+        <div className="container-header">
+          <div className="logo">Taverna |</div>
           <div className="nav-bar">
             <button className="button nav-button-one">Get Items</button>
-
             <button className="button nav-button-two">Create Item</button>
           </div>
         </div>
 
-        <div className="item-list">{renderItems()}</div>
-
-        <div className="content">Content</div>
-      </div>{" "}
-      /*END GRID CONTAINER */
+        <div className ="container-body">
+          <div className="item-list"><MenuItems items={items}/></div>
+          <div className="content">Content</div>
+        </div>
+      </div>
     </div>
   );
 }
