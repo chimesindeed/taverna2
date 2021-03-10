211030e45d44630ecbdc765c44a23ec3d7df2be1
chimesindeed <gratitudeadore@fastmail.com>
2021-03-09 19:38:06 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
-
+const url = "localhost:3001/menus";
 export default function App() {
-  const renderItems =()=> {
+  const [items, setItems]=useState("");
+
+  const renderItems = () => {
+    if(items ===""){return "loading"
+    }
+    else {
+      return(
+        <ul>
+        {
+          items.map((item)=>{
+          return(<li>item.item</li>)
+        })
+        }
+        </ul>
+      )
+    }
 
   }
   return (
-    <div className="container">
-      
-      <div className="header">
-        
-        <div className="logo">Taverna |</div>
-        
-        <div className="nav-bar">
-       
-          <button className="button nav-button-one">Get Items</button>
-          
-          <button className="button nav-button-two">Create Item</button>
-       
-       </div> 
-      
-      </div>
-      
-      <div className="item-list">
-        {
-          renderItems()
-        }  
-      </div>
-      
-      <div className="content">Content</div>
-    
+    <div className="main">
+      <div className="container">
+        <div className="header">
+          <div className="logo">Taverna |</div>
+
+          <div className="nav-bar">
+            <button className="button nav-button-one">Get Items</button>
+
+            <button className="button nav-button-two">Create Item</button>
+          </div>
+        </div>
+
+        <div className="item-list">{renderItems()}</div>
+
+        <div className="content">Content</div>
+      </div>{" "}
+      /*END GRID CONTAINER */
     </div>
   );
 }
