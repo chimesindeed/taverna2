a7b354147f6e94d6d8e4312470e1e12a5deec3ae
dimitri.gedda <gratitudeadore@fastmail.com>
2021-03-10 04:57:14 -0500

    MenuItems receives items as props from App.js and renders

---
+++ src/MenuItems.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const MenuItems = (props) => {
+  
+  function displayItems(items){
+    return (
+      items
+        ?
+          <ul>
+            { items.map(item=><li>{ item.item }</li>)}
+          </ul>
+        : 
+          <h3>Loading...</h3>
+    )
+  }
+  
+  return(
+    <div>
+      {displayItems(props.items)}
+    </div>
+  )
+ }
